864d18bc3678ecdf6f8a75648fadd7d80632e4ac
5um4n7h <sumanthhegde13@gmail.com>
2021-06-01 13:52:54 +0530

    Initial commit

---
+++ MAD01_Visiting_Card/app/src/main/java/com/sumanth/mad01_visiting_card/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sumanth.mad01_visiting_card;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+}
